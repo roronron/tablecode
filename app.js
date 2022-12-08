b2f93dd8c32602bbd02bfc330cbc28ba0a64990f
Ronald Brown <rbrown780@gmail.com>
2022-12-08 08:04:33 +0700

    add javascirpt

---
+++ app.js
@@ -0,0 +1,38 @@
+"strict";
+ 
+  function copya(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyb(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyc(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyd(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copye(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyf(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyg(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyh(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyi(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
