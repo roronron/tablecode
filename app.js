530fd4f4a9cf23c298a50a5f08e5ff22ab07d91d
Ronald Brown <rbrown780@gmail.com>
2022-12-08 11:41:05 +0700

    update al

--- app.js
+++ app.js
@@ -36,3 +36,201 @@
     document.getElementById(id).select();
     document.execCommand("copy");
   }
+  function copyj(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyk(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+
+  function copyl(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copym(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyo(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyp(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+
+
+  function copyq(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyr(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copys(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyt(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+
+
+  function copyu(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyv(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyw(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyx(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+
+  }
+
+
+
+  function copyy(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyz(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyaa(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyab(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+    function copyac(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyad(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyae(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyaf(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+  
+  function copyag(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyah(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyai(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyaj(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+
+;
+
+  function copyak(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyal(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyam(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyan(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+
+  function copyao(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyap(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyaq(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyar(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+  function copyas(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyat(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyau(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyav(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+
+
+  function copyaw(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+
+  function copyax(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
+  function copyay(id) {
+    document.getElementById(id).select();
+    document.execCommand("copy");
+  }
